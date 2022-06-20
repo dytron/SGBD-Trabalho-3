c67cbdc4b3774196b89874f0c495cedc6bbc98cf
dytron <guilhermealfaiagui1@gmail.com>
2022-06-20 09:55:10 -0300

    Modificação no protótipo

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "managers.hpp"
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
--- managers.hpp
+++ managers.hpp
@@ -4,15 +4,20 @@
 #include <cstdio>
 #include <bitset>
 #include <vector>
+#include <queue>
 #include <math.h>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
 #include <filesystem>
 
+enum LOCK { SHARED, EXCLUSIVE };
+enum STATE { ACTIVE, COMMITED, ABORTED };
+
 using namespace std;
 // duvidas
-// sempre que uma transacao e criada ela recebe um numero - mas sempre que uma transacao e criada ela recebe um timestamp - esses valores nao vao ser sempre iguais?
+// sempre que uma transacao e criada ela recebe um numero - mas sempre que uma transacao e criada ela recebe um timestamp
+// esses valores nao vao ser sempre iguais?
 class TransactionManager
 {
 public:
@@ -33,13 +38,34 @@ public:
 class Transaction
 {
 public:
+    // Identificador
     int Id;
+    // Timestamp
     int Ts;
-    string state;
-    vector<int> waitForList;
+    // Estado da Transação
+    STATE state;
+    // Fila de <Transação, Modo de bloqueio>
+    queue<pair<int, LOCK>> waitForList;
 };
 
 class LockManager
 {
 public:
+    // Insere um bloqueio no modo compartilhado na Lock Table sobre o item D para a transação Tr se puder,
+    // Caso contrário cria/atualiza a Wait Q de D com a transação Tr */
+    void LS(Transaction Tr, int D)
+    {
+
+    }
+    // Insere um bloqueio no modo exclusivo na Lock_Table sobre o item D para a transação Tr
+    // Caso contrário cria/atualiza a Wait Q de D com a transação Tr */
+    void LX(Transaction Tr, int D)
+    {
+
+    }
+    // Apaga o bloqueio da transação Tr sobre o item D
+    void U(Transaction Tr, int D)
+    {
+
+    }
 };
