2329bec1502f890c46d388ad493e4994e9815c29
dytron <guilhermealfaiagui1@gmail.com>
2022-06-20 11:31:17 -0300

    Correção nas classes

--- managers.hpp
+++ managers.hpp
@@ -44,13 +44,17 @@ public:
     int Ts;
     // Estado da Transação
     STATE state;
-    // Fila de <Transação, Modo de bloqueio>
-    queue<pair<int, LOCK>> waitForList;
+    // Lista de Transações pelas quais esta espera
+    vector<int> waitForList;
 };
 
 class LockManager
 {
 public:
+    // Tabela de bloqueios sobre os itens de dados
+    LockTable lockTable;
+    // Lista que contém, para cada item de dado, uma fila de transação e modo de bloqueio
+    vector< queue< pair<int, LOCK> > > waitForDataList;
     // Insere um bloqueio no modo compartilhado na Lock Table sobre o item D para a transação Tr se puder,
     // Caso contrário cria/atualiza a Wait Q de D com a transação Tr
     void LS(Transaction Tr, int D)
@@ -65,6 +69,18 @@ public:
     }
     // Apaga o bloqueio da transação Tr sobre o item D
     void U(Transaction Tr, int D)
+    {
+
+    }
+};
+class LockTable
+{
+public:
+    void addLock()
+    {
+
+    }
+    void removeLock()
     {
 
     }
